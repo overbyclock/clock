b228b40a10271fbed2220f6f4e52f5afe029fa51
Overbyclock <overbyclock@gmail.com>
2021-06-21 16:38:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const min = now.getMinutes();
+  const minDegress = ((min / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minDegress}deg)`;
+
+  const hour = now.getHours();
+  const hourDegress = ((hour / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegress}deg)`;
+}
+setInterval(setDate, 1000,);
